89ff3aeaed8e67d1b1951a55531abce1947436a9
romain-bourjot-qare <romain.bourjot@qare.io>
2020-10-12 17:04:48 +0200

    wip

--- src/levels.js
+++ src/levels.js
@@ -1,26 +1,10 @@
 // @flow
 
-export const LEVEL_TRACE: string = 'trace';
-export const LEVEL_DEBUG: string = 'debug';
-export const LEVEL_INFO: string = 'info';
-export const LEVEL_WARN: string = 'warn';
-export const LEVEL_ERROR: string = 'error';
-export const LEVEL_FATAL: string = 'fatal';
-
-export const LEVELS: Array<string> = [
-  LEVEL_TRACE,
-  LEVEL_DEBUG,
-  LEVEL_INFO,
-  LEVEL_WARN,
-  LEVEL_ERROR,
-  LEVEL_FATAL,
-];
-
-export default Object.freeze({
-  LEVEL_TRACE,
-  LEVEL_DEBUG,
-  LEVEL_INFO,
-  LEVEL_WARN,
-  LEVEL_ERROR,
-  LEVEL_FATAL,
+export const levels = Object.freeze({
+  trace: 'trace',
+  debug: 'debug',
+  info: 'info',
+  warn: 'warn',
+  error: 'error',
+  fatal: 'fatal',
 });
