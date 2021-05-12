76066606d6a9314122895c6945b1e7709269cd86
Remy EUSBEE <remyeusebe@gmail.com>
2021-05-12 15:58:20 +0200

    add default format error that return stacktrace

--- dist/index.js
+++ dist/index.js
@@ -3,10 +3,13 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.formatError = formatError;
 exports.init = init;
 exports["default"] = void 0;
 
 var _pino = _interopRequireDefault(require("pino"));
+
+var _util = require("util");
 
 var _levels = _interopRequireDefault(require("./levels"));
 
@@ -17,6 +20,17 @@ function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (O
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function formatError(input) {
+  return {
+    stacktrace: (0, _util.inspect)(input, {
+      showHidden: false,
+      colors: false,
+      depth: 3,
+      breakLength: Infinity
+    })
+  };
+}
 
 var defaultConfig = {
   logger: {
